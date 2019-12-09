56611100836ba945ee4487d5ec1ba901eb17a7e2
Dylan Hicks <dman1366@gmail.com>
2019-12-09 12:40:20 -0700

    Starting day 3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod file_reader;
 
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn print_seperator() {
     println!("-------------------------------");
@@ -11,6 +12,7 @@ fn run_day(day: usize) {
     match day {
         1 => day_1::run_day_1(),
         2 => day_2::run_day_2(),
+        3 => day_3::run_day_3(),
         _ => (),
     }
 }
@@ -18,7 +20,7 @@ fn run_day(day: usize) {
 fn main() {
     print_seperator();
 
-    run_day(2);
+    run_day(3);
 
     print_seperator();
 }
