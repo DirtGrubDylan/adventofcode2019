b81312e445b6c1212cda912cfdd64c574bd30030
Dylan Hicks <dman1366@gmail.com>
2019-12-29 22:26:57 -0700

    Finished Day 5!

--- src/day_2/intcode_instruction.rs
+++ src/day_2/intcode_instruction.rs
@@ -20,6 +20,10 @@ pub enum Opcode {
     Multiply(Parameter, Parameter, Parameter),
     SaveInput(Parameter, Parameter),
     Output(Parameter),
+    JumpIfTrue(Parameter, Parameter),
+    JumpIfFalse(Parameter, Parameter),
+    StoreIfLessThan(Parameter, Parameter, Parameter),
+    StoreIfEquals(Parameter, Parameter, Parameter),
     Terminate,
 }
 
@@ -74,6 +78,48 @@ impl Opcode {
                     Parameter::new(first_parameter_mode, program_memory[current_index + 1]);
 
                 Opcode::Output(first_parameter)
+            }
+            5 => {
+                let first_parameter =
+                    Parameter::new(first_parameter_mode, program_memory[current_index + 1]);
+
+                let second_parameter =
+                    Parameter::new(second_parameter_mode, program_memory[current_index + 2]);
+
+                Opcode::JumpIfTrue(first_parameter, second_parameter)
+            }
+            6 => {
+                let first_parameter =
+                    Parameter::new(first_parameter_mode, program_memory[current_index + 1]);
+
+                let second_parameter =
+                    Parameter::new(second_parameter_mode, program_memory[current_index + 2]);
+
+                Opcode::JumpIfFalse(first_parameter, second_parameter)
+            }
+            7 => {
+                let first_parameter =
+                    Parameter::new(first_parameter_mode, program_memory[current_index + 1]);
+
+                let second_parameter =
+                    Parameter::new(second_parameter_mode, program_memory[current_index + 2]);
+
+                let third_parameter =
+                    Parameter::new(third_parameter_mode, program_memory[current_index + 3]);
+
+                Opcode::StoreIfLessThan(first_parameter, second_parameter, third_parameter)
+            }
+            8 => {
+                let first_parameter =
+                    Parameter::new(first_parameter_mode, program_memory[current_index + 1]);
+
+                let second_parameter =
+                    Parameter::new(second_parameter_mode, program_memory[current_index + 2]);
+
+                let third_parameter =
+                    Parameter::new(third_parameter_mode, program_memory[current_index + 3]);
+
+                Opcode::StoreIfEquals(first_parameter, second_parameter, third_parameter)
             }
             99 => Opcode::Terminate,
             _ => panic!("Unexpected opcode given!"),
@@ -148,6 +194,88 @@ impl Opcode {
                     Self::get_parameter_value_from_memory(first_parameter, program_memory);
 
                 Some((output_value, current_index + 2))
+            }
+            Opcode::JumpIfTrue(first_parameter, second_parameter) => {
+                let first_value =
+                    Self::get_parameter_value_from_memory(first_parameter, program_memory);
+
+                let second_value =
+                    Self::get_parameter_value_from_memory(second_parameter, program_memory);
+
+                let mut success_value = 0;
+                let mut next_index = current_index + 3;
+
+                if first_value != 0 {
+                    success_value = 1;
+                    next_index = second_value as usize;
+                }
+
+                Some((success_value, next_index))
+            }
+            Opcode::JumpIfFalse(first_parameter, second_parameter) => {
+                let first_value =
+                    Self::get_parameter_value_from_memory(first_parameter, program_memory);
+
+                let second_value =
+                    Self::get_parameter_value_from_memory(second_parameter, program_memory);
+
+                let mut success_value = 0;
+                let mut next_index = current_index + 3;
+
+                if first_value == 0 {
+                    success_value = 1;
+                    next_index = second_value as usize;
+                }
+
+                Some((success_value, next_index))
+            }
+            Opcode::StoreIfLessThan(first_parameter, second_parameter, third_parameter) => {
+                let first_value =
+                    Self::get_parameter_value_from_memory(first_parameter, program_memory);
+
+                let second_value =
+                    Self::get_parameter_value_from_memory(second_parameter, program_memory);
+
+                let save_index = match third_parameter {
+                    Parameter::Position(index) => Self::transform_index(*index, program_memory),
+                    Parameter::Immediate(_) => {
+                        panic!("Cannot save a value with an immediate parameter!")
+                    }
+                };
+
+                let mut success_value = 0;
+
+                if first_value < second_value {
+                    success_value = 1;
+                }
+
+                program_memory[save_index] = success_value;
+
+                Some((success_value, current_index + 4))
+            }
+            Opcode::StoreIfEquals(first_parameter, second_parameter, third_parameter) => {
+                let first_value =
+                    Self::get_parameter_value_from_memory(first_parameter, program_memory);
+
+                let second_value =
+                    Self::get_parameter_value_from_memory(second_parameter, program_memory);
+
+                let save_index = match third_parameter {
+                    Parameter::Position(index) => Self::transform_index(*index, program_memory),
+                    Parameter::Immediate(_) => {
+                        panic!("Cannot save a value with an immediate parameter!")
+                    }
+                };
+
+                let mut success_value = 0;
+
+                if first_value == second_value {
+                    success_value = 1;
+                }
+
+                program_memory[save_index] = success_value;
+
+                Some((success_value, current_index + 4))
             }
             Opcode::Terminate => None,
         }
@@ -237,6 +365,64 @@ mod tests {
     }
 
     #[test]
+    fn test_new_jump_if_true() {
+        let program_memory = vec![5, 4, 5, 4, -1, 99];
+        let user_input = 1;
+        let current_index = 0;
+
+        let expected = Opcode::JumpIfTrue(Parameter::Position(4), Parameter::Position(5));
+
+        let result = Opcode::new(user_input, &program_memory, current_index);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_new_jump_if_false() {
+        let program_memory = vec![106, 4, 5, 4, -1, 99];
+        let user_input = 1;
+        let current_index = 0;
+
+        let expected = Opcode::JumpIfFalse(Parameter::Immediate(4), Parameter::Position(5));
+
+        let result = Opcode::new(user_input, &program_memory, current_index);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_new_store_if_less_than() {
+        let program_memory = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
+        let user_input = 1;
+        let current_index = 2;
+
+        let expected = Opcode::StoreIfLessThan(
+            Parameter::Position(9),
+            Parameter::Position(10),
+            Parameter::Position(9));
+
+        let result = Opcode::new(user_input, &program_memory, current_index);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_new_store_if_equals() {
+        let program_memory = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
+        let user_input = 1;
+        let current_index = 2;
+
+        let expected = Opcode::StoreIfEquals(
+            Parameter::Immediate(-1),
+            Parameter::Immediate(8),
+            Parameter::Position(3));
+
+        let result = Opcode::new(user_input, &program_memory, current_index);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
     fn test_new_terminate() {
         let program_memory = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
         let user_input = 1;
@@ -306,6 +492,134 @@ mod tests {
 
         let expected_output = Some((3, 4));
         let expected_program_memory = vec![3, 0, 4, 0, 99];
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_jump_if_true() {
+        let mut program_memory = vec![3, 3, 1105, 1, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((1, 9));
+        let expected_program_memory = program_memory.clone();
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_no_jump_if_true() {
+        let mut program_memory = vec![3, 3, 5, 6, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((0, 5));
+        let expected_program_memory = program_memory.clone();
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_jump_if_false() {
+        let mut program_memory = vec![3, 3, 1106, 0, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((1, 9));
+        let expected_program_memory = program_memory.clone();
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_no_jump_if_false() {
+        let mut program_memory = vec![3, 3, 6, 6, 9, 1101, 1, 0, 12, 4, 12, 99, 1];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((0, 5));
+        let expected_program_memory = program_memory.clone();
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_store_if_less_than() {
+        let mut program_memory = vec![3, 3, 1107, 7, 8, 3, 4, 3, 99];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((1, 6));
+        let expected_program_memory = vec![3, 3, 1107, 1, 8, 3, 4, 3, 99];
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_no_store_if_less_than() {
+        let mut program_memory = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, 8, 8];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((0, 6));
+        let expected_program_memory = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, 0, 8];
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_store_if_equals() {
+        let mut program_memory = vec![3, 3, 1108, 8, 8, 3, 4, 3, 99];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((1, 6));
+        let expected_program_memory = vec![3, 3, 1108, 1, 8, 3, 4, 3, 99];
+
+        let result = opcode.execute(&mut program_memory, current_index);
+
+        assert_eq!(result, expected_output);
+        assert_eq!(program_memory, expected_program_memory);
+    }
+
+    #[test]
+    fn test_execute_no_store_if_equals() {
+        let mut program_memory = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
+        let user_input = 1;
+        let current_index = 2;
+        let opcode = Opcode::new(user_input, &program_memory, current_index);
+
+        let expected_output = Some((0, 6));
+        let expected_program_memory = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8];
 
         let result = opcode.execute(&mut program_memory, current_index);
 
--- src/day_5/mod.rs
+++ src/day_5/mod.rs
@@ -9,27 +9,22 @@ pub fn run_day_5() {
     match file_input {
         Ok(programs) => {
             if let Some(program) = programs.get(0) {
-                find_first_solution(&program);
+                let program_values: Vec<String> =
+                    program.split(",").map(|s| String::from(s)).collect();
+
+                let user_input = get_user_input();
+                let intcode_computer = IntcodeComputer::from(program_values.as_slice());
+
+                let (_, output_values) = intcode_computer.run_program(user_input);
+
+                println!("System output values are: {:?}", output_values);
             }
         }
         Err(error) => println!("Error parsing file: {:?}", error),
     }
 }
 
-pub fn find_first_solution(program: &str) {
-    let program_values: Vec<String> = program.split(",").map(|s| String::from(s)).collect();
-
-    let user_input = get_user_input();
-    let intcode_computer = IntcodeComputer::from(program_values.as_slice());
-
-    println!("Diagnosing system ID: {}", user_input);
-
-    let (_, output_values) = intcode_computer.run_program(user_input);
-
-    println!("Part 1 output_values are: {:?}", output_values);
-}
-
-pub fn get_user_input() -> i32 {
+fn get_user_input() -> i32 {
     let mut input_buffer = String::new();
 
     print!("Please input a system ID to diagnose: ");
