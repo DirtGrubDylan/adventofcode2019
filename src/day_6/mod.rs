6716ba85e3861dff12863d13050fda38ecbe2d2a
Dylan Hicks <dman1366@gmail.com>
2020-01-10 13:02:01 -0700

    Finish day 6 part 1

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -1,5 +1,28 @@
 pub mod orbit;
 
+use crate::file_reader::to_string_vector;
+use orbit::OrbitMap;
+
 pub fn run_day_6() {
-    unimplemented!()
+    let file_input = to_string_vector("inputs/day_6.txt");
+
+    match file_input {
+        Ok(orbit_description) => {
+            run_part_1(&orbit_description);
+        }
+        Err(error) => panic!("Error parsing file: {:?}", error),
+    }
+}
+
+fn run_part_1(orbit_description: &[String]) {
+    let mut orbit_map = OrbitMap::new();
+
+    orbit_map.add_orbit_description(orbit_description);
+
+    let total_number_of_direct_and_indirect_orbits = orbit_map.total_number_of_orbits();
+
+    println!(
+        "Day 6 Part 1 Solution: {}",
+        total_number_of_direct_and_indirect_orbits
+    );
 }
--- src/day_6/orbit.rs
+++ src/day_6/orbit.rs
@@ -1,10 +1,47 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::panic;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct OrbitObject {
     pub name: String,
-    pub object_names_directly_orbiting: Vec<String>,
+    pub object_name_this_is_orbiting: Option<String>,
+    pub object_names_directly_orbiting_this: HashSet<String>,
+}
+
+impl OrbitObject {
+    pub fn new(
+        name: &str,
+        object_name_this_is_orbiting: Option<&str>,
+        object_names_directly_orbiting_this: &[&str],
+    ) -> OrbitObject {
+        let temp_set = object_names_directly_orbiting_this
+            .iter()
+            .map(|&s| String::from(s))
+            .collect();
+
+        let temp_object_name_this_is_orbiting =
+            object_name_this_is_orbiting.map(|s| String::from(s));
+
+        OrbitObject {
+            name: String::from(name),
+            object_name_this_is_orbiting: temp_object_name_this_is_orbiting,
+            object_names_directly_orbiting_this: temp_set,
+        }
+    }
+
+    pub fn add_new_object_name_that_directly_orbits(
+        &mut self,
+        new_directly_orbiting_object_name: &str,
+    ) {
+        self.object_names_directly_orbiting_this
+            .insert(String::from(new_directly_orbiting_object_name));
+    }
+
+    pub fn set_object_name_this_is_orbiting(&mut self, object_name_this_is_orbiting: &str) {
+        if self.object_name_this_is_orbiting.is_none() {
+            self.object_name_this_is_orbiting = Some(String::from(object_name_this_is_orbiting));
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -20,31 +57,80 @@ impl OrbitMap {
     }
 
     pub fn add_orbit_description(&mut self, orbit_description: &[String]) {
-        unimplemented!()
+        orbit_description
+            .iter()
+            .for_each(|s| self.add_orbit_description_line(&s));
     }
 
     pub fn total_number_of_orbits(&self) -> u32 {
-        unimplemented!()
+        self.object_orbiting_map.keys().fold(0, |acc, object_name| {
+            acc + self.number_of_orbits_for(object_name)
+        })
     }
 
     fn add_orbit_description_line(&mut self, orbit_description_line: &str) {
-        unimplemented!()
-    }
+        let object_names: Vec<String> = orbit_description_line
+            .split(')')
+            .map(|s| String::from(s))
+            .collect();
 
-    fn total_number_of_direct_orbits(&self) -> u32 {
-        unimplemented!()
-    }
+        assert!(object_names.len() == 2);
 
-    fn total_number_of_indirect_orbits(&self) -> u32 {
-        unimplemented!()
+        let first_object_name = object_names[0].clone();
+        let second_object_name = object_names[1].clone();
+
+        let first_object = self
+            .object_orbiting_map
+            .entry(first_object_name.clone())
+            .or_insert(OrbitObject::new(&first_object_name, None, &[]));
+
+        (*first_object).add_new_object_name_that_directly_orbits(&second_object_name);
+
+        let second_object = self
+            .object_orbiting_map
+            .entry(second_object_name.clone())
+            .or_insert(OrbitObject::new(
+                &second_object_name,
+                Some(&first_object_name),
+                &[],
+            ));
+
+        (*second_object).set_object_name_this_is_orbiting(&first_object_name);
     }
 
     fn number_of_direct_orbits_for(&self, object_name: &str) -> u32 {
-        unimplemented!()
+        if let Some(object) = self.object_orbiting_map.get(object_name) {
+            object.object_name_this_is_orbiting.is_some() as u32
+        } else {
+            0
+        }
     }
 
     fn number_of_indirect_orbits_for(&self, object_name: &str) -> u32 {
-        unimplemented!()
+        let mut number_of_indirect_orbits = 0;
+        let mut optional_next_object_name: Option<String> = None;
+
+        // Do not count directly orbiting objects
+        if let Some(object) = self.object_orbiting_map.get(object_name) {
+            optional_next_object_name = object.object_name_this_is_orbiting.clone();
+        }
+
+        while let Some(next_object_name) = optional_next_object_name.clone() {
+            if let Some(object) = self.object_orbiting_map.get(&next_object_name) {
+                optional_next_object_name = object.object_name_this_is_orbiting.clone();
+            }
+
+            if optional_next_object_name.is_some() {
+                number_of_indirect_orbits += 1;
+            }
+        }
+
+        number_of_indirect_orbits as u32
+    }
+
+    fn number_of_orbits_for(&self, object_name: &str) -> u32 {
+        self.number_of_direct_orbits_for(object_name)
+            + self.number_of_indirect_orbits_for(object_name)
     }
 }
 
@@ -58,55 +144,184 @@ mod tests {
 
     #[test]
     fn test_add_one_orbit_description_line() {
-        unimplemented!()
+        let mut orbit_map = OrbitMap::new();
+
+        let expected: HashMap<String, OrbitObject> = [
+            (String::from("COM"), OrbitObject::new("COM", None, &["B"])),
+            (String::from("B"), OrbitObject::new("B", Some("COM"), &[])),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        orbit_map.add_orbit_description_line(TEST_ORBIT_DESCRIPTION[0]);
+
+        assert_eq!(orbit_map.object_orbiting_map, expected);
+    }
+
+    #[test]
+    fn test_add_two_orbit_description_line() {
+        let mut orbit_map = OrbitMap::new();
+
+        let expected: HashMap<String, OrbitObject> = [
+            (
+                String::from("COM"),
+                OrbitObject::new("COM", None, &["B", "C"]),
+            ),
+            (String::from("B"), OrbitObject::new("B", Some("COM"), &[])),
+            (String::from("C"), OrbitObject::new("C", Some("COM"), &[])),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        orbit_map.add_orbit_description_line(TEST_ORBIT_DESCRIPTION[0]);
+        orbit_map.add_orbit_description_line("COM)C");
+
+        assert_eq!(orbit_map.object_orbiting_map, expected);
     }
 
     #[test]
     fn test_add_orbit_description() {
-        unimplemented!()
+        run_test(|orbit_map: OrbitMap| {
+            let expected: HashMap<String, OrbitObject> = [
+                (String::from("COM"), OrbitObject::new("COM", None, &["B"])),
+                (
+                    String::from("B"),
+                    OrbitObject::new("B", Some("COM"), &["C", "G"]),
+                ),
+                (String::from("C"), OrbitObject::new("C", Some("B"), &["D"])),
+                (
+                    String::from("D"),
+                    OrbitObject::new("D", Some("C"), &["E", "I"]),
+                ),
+                (
+                    String::from("E"),
+                    OrbitObject::new("E", Some("D"), &["F", "J"]),
+                ),
+                (String::from("F"), OrbitObject::new("F", Some("E"), &[])),
+                (String::from("G"), OrbitObject::new("G", Some("B"), &["H"])),
+                (String::from("H"), OrbitObject::new("H", Some("G"), &[])),
+                (String::from("I"), OrbitObject::new("I", Some("D"), &[])),
+                (String::from("J"), OrbitObject::new("J", Some("E"), &["K"])),
+                (String::from("K"), OrbitObject::new("K", Some("J"), &["L"])),
+                (String::from("L"), OrbitObject::new("L", Some("K"), &[])),
+            ]
+            .iter()
+            .cloned()
+            .collect();
+
+            assert_eq!(orbit_map.object_orbiting_map, expected);
+        });
     }
 
     #[test]
-    fn test_number_of_direct_orbits_for() {
+    fn test_number_of_direct_orbits_for_b() {
         run_test(|orbit_map: OrbitMap| {
-            let expected = 42;
+            let expected = 1;
 
-            let result = orbit_map.total_number_of_orbits();
+            let result = orbit_map.number_of_direct_orbits_for("B");
 
-            assert_eq!(result, expected)
+            assert_eq!(result, expected);
         })
     }
 
     #[test]
-    fn test_number_of_indirect_orbits_for() {
+    fn test_number_of_direct_orbits_for_q() {
         run_test(|orbit_map: OrbitMap| {
-            let expected = 42;
+            let expected = 0;
 
-            let result = orbit_map.total_number_of_orbits();
+            let result = orbit_map.number_of_direct_orbits_for("Q");
 
-            assert_eq!(result, expected)
+            assert_eq!(result, expected);
         })
     }
 
     #[test]
-    fn test_total_number_of_direct_orbits() {
+    fn test_number_of_indirect_orbits_for_d() {
         run_test(|orbit_map: OrbitMap| {
-            let expected = 11;
+            let expected = 2;
 
-            let result = orbit_map.total_number_of_direct_orbits();
+            let result = orbit_map.number_of_indirect_orbits_for("D");
 
-            assert_eq!(result, expected)
+            assert_eq!(result, expected);
         })
     }
 
     #[test]
-    fn test_total_number_of_indirect_orbits() {
+    fn test_number_of_indirect_orbits_for_l() {
         run_test(|orbit_map: OrbitMap| {
-            let expected = 31;
+            let expected = 6;
 
-            let result = orbit_map.total_number_of_indirect_orbits();
+            let result = orbit_map.number_of_indirect_orbits_for("L");
 
-            assert_eq!(result, expected)
+            assert_eq!(result, expected);
+        })
+    }
+
+    #[test]
+    fn test_number_of_indirect_orbits_for_com() {
+        run_test(|orbit_map: OrbitMap| {
+            let expected = 0;
+
+            let result = orbit_map.number_of_indirect_orbits_for("COM");
+
+            assert_eq!(result, expected);
+        })
+    }
+
+    #[test]
+    fn test_number_of_indirect_orbits_for_q() {
+        run_test(|orbit_map: OrbitMap| {
+            let expected = 0;
+
+            let result = orbit_map.number_of_indirect_orbits_for("Q");
+
+            assert_eq!(result, expected);
+        })
+    }
+
+    #[test]
+    fn test_number_of_orbits_for_d() {
+        run_test(|orbit_map: OrbitMap| {
+            let expected = 3;
+
+            let result = orbit_map.number_of_orbits_for("D");
+
+            assert_eq!(result, expected);
+        })
+    }
+
+    #[test]
+    fn test_number_of_orbits_for_l() {
+        run_test(|orbit_map: OrbitMap| {
+            let expected = 7;
+
+            let result = orbit_map.number_of_orbits_for("L");
+
+            assert_eq!(result, expected);
+        })
+    }
+
+    #[test]
+    fn test_number_of_orbits_for_com() {
+        run_test(|orbit_map: OrbitMap| {
+            let expected = 0;
+
+            let result = orbit_map.number_of_orbits_for("COM");
+
+            assert_eq!(result, expected);
+        })
+    }
+
+    #[test]
+    fn test_number_of_orbits_for_q() {
+        run_test(|orbit_map: OrbitMap| {
+            let expected = 0;
+
+            let result = orbit_map.number_of_orbits_for("Q");
+
+            assert_eq!(result, expected);
         })
     }
 
@@ -117,7 +332,7 @@ mod tests {
 
             let result = orbit_map.total_number_of_orbits();
 
-            assert_eq!(result, expected)
+            assert_eq!(result, expected);
         })
     }
 
@@ -125,10 +340,17 @@ mod tests {
     where
         T: FnOnce(OrbitMap) -> () + panic::UnwindSafe,
     {
-        let orbit_map = OrbitMap::new();
+        let mut orbit_map = OrbitMap::new();
+        let orbit_description: Vec<String> = TEST_ORBIT_DESCRIPTION
+            .iter()
+            .rev()
+            .map(|&s| String::from(s))
+            .collect();
+
+        orbit_map.add_orbit_description(&orbit_description);
 
         let result = panic::catch_unwind(|| test(orbit_map));
 
-        assert!(result.is_ok())
+        assert!(result.is_ok());
     }
 }
