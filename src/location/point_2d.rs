52d616ae55f9fb8d088bad109dd3f7992cb76847
Dylan Hicks <dman1366@gmail.com>
2020-02-27 08:15:51 -0700

    Test the Point2d struct.

--- src/location/point_2d.rs
+++ src/location/point_2d.rs
@@ -11,6 +11,15 @@ where
     pub y: T,
 }
 
+impl<T> Point2d<T>
+where
+    T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + Into<f64> + Copy,
+{
+    pub fn new(x: T, y: T) -> Point2d<T> {
+        Point2d { x, y }
+    }
+}
+
 impl<T> Location for Point2d<T>
 where
     T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + Into<f64> + Copy,
@@ -31,5 +40,36 @@ where
         let temp = (relative_x * relative_x + relative_y * relative_y).into();
 
         temp.sqrt()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EPSILON: f64 = 1e-10;
+
+    const ORIGIN_POINT: Point2d<i32> = Point2d { x: 0, y: 0 };
+
+    #[test]
+    fn test_manhattan_distance_to() {
+        let point = Point2d::new(5, 5);
+
+        let expected = 10;
+
+        let result = ORIGIN_POINT.manhattan_distance_to(&point);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_distance_to() {
+        let point = Point2d::new(3, 4);
+
+        let expected = 5.0;
+
+        let result = ORIGIN_POINT.distance_to(&point);
+
+        assert!((result - expected).abs() < EPSILON);
     }
 }
